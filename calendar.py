60f37169c4425e89219cebd30bb501dba2ad9204
Bayesd <gbgleo@gmail.com>
2020-12-28 22:55:03 +0100

    Nödlösning som fungerar i 25 fall av 35

--- calendar.py
+++ calendar.py
@@ -4,20 +4,15 @@ from datetime import date
 input = os.popen("cal")
 calendar = input.read()
 rows = calendar.splitlines()
-
 new_cal =""
 counter = 0
-todays_date = str(date.today())[-2:]
-# Combining the rows back to a string
+todays_date = int(str(date.today())[-2:])
 for row in rows:
     row_str = str(rows[counter])
     new_cal += row_str + "\n"
     counter += 1
-
-dates = new_cal[44:].replace("28", f"({todays_date})")
-print(dates)
-
-final_cal = new_cal.replace("28", "fart")
-print(final_cal)
-print(todays_date)
-
+top_cal = new_cal[:45]
+date_cal = new_cal[44:]
+date_cal = date_cal.replace(str(todays_date - 1), f"{str(todays_date - 1)} (")
+date_cal = date_cal.replace(str(todays_date + 1), f") {str(todays_date + 1)}")
+print(top_cal + date_cal)
