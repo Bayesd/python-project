2d2f5d48216e14d3dc894e4ab5fcf55ad53932b8
Bayesd <gbgleo@gmail.com>
2020-12-28 16:05:29 +0100

    Commit from Ubuntu WML

---
+++ nameday.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import requests
+
+def append_file():
+    file = open("namnsdag.txt", "a+")
+    file.write(get_todays_name_and_time())
+
+def get_todays_name_and_time():
+    r = requests.get("https://sholiday.faboul.se/dagar/v2.1/")
+    my_dict = r.json()
+    dict_in_dict = my_dict["dagar"][0]
+    name = dict_in_dict["namnsdag"][0]
+    time = my_dict["cachetid"]
+    return f"{time}\nDagens namnsdag: {name}\n\n"
+
+append_file()
+
